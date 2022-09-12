efbe135561033b85ac24f138fb714a9ebab140c2
kerry3 <36907312+kerry3@users.noreply.github.com>
2022-09-12 11:14:47 +0900

    Update bounce.py
    https://opga009.com/bbs/login.php
    개구리 사이트로 첫번째 url 업데이트
    
    로그인 부분 주석 처리

--- bounce.py
+++ bounce.py
@@ -14,11 +14,10 @@ options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 # 혹은 options.add_argument("--disable-gpu")
 
+"""
 driver = webdriver.Chrome('chromedriver', chrome_options=options)
-url='https://opga100.com/bbs/login.php' #들어가려는 사이트 주소
 
-#1.26 web driver 버전 바뀌어서 다시 깔아줌. 코드랑 같은 폴더에 있게 하자.
-
+url='https://opga009.com/bbs/login.php' #들어가려는 사이트 주소
 driver.get(url)  #사이트 접속
 
 driver.find_element_by_id('login_id').send_keys('mynamehaha') #id로 찾음, 개발자 도구로 login_id가지고 있다는거 알아내고 그거로 접근. send_key로 아이디 myname haha를 쏴줌 
@@ -27,8 +26,10 @@ driver.find_element_by_id('login_pw').send_keys('123123')
 driver.find_element_by_xpath('/html/body/div[6]/div/div/div/div/div/div/div[2]/div[1]/div/div/form/button').click() # 로그인 버튼 클릭. xpath로 접근. 버튼에 우클릭 하고 xpath 찾기 하면됨. click함수로 클릭시킴.
 #1.26 이거 element값 바뀌어서 다시 xpath따서 변경해줌
 
+"""
+# 로그인 부분(바운스는 로그인 안해도 출근부 조회 가능해서 그냥 다 주석 처리)
 
-url='https://opga100.com/bbs/board.php?bo_table=op_partner_posting&wr_id=537552&uptime=1658536907&cat=18&cat2=&addrName=&sub_bs=&_stx=%EC%84%A0%EB%A6%89-%EB%B0%94%EC%9A%B4%EC%8A%A4'
+url='https://opga009.com/bbs/board.php?bo_table=op_partner_posting&wr_id=537552&uptime=1662946218&cat=1&cat2=6&addrName=%EC%84%A0%EB%A6%89&biz=1&bizName=%EC%98%A4%ED%94%BC&_stx=%EC%84%A0%EB%A6%89-%EB%B0%94%EC%9A%B4%EC%8A%A4&fin=Y'
 driver.get(url)
 
 html = driver.page_source
